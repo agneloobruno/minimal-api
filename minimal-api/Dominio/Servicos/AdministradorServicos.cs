d62a380e31905b4f7abc0deb3d28f2a43450f22c
Bruno Agnelo De Almeida <3714194@empresa.mti.mt.gov.br>
2025-10-10 16:58:44 -0400

    Implementa serviço de autenticação para administradores e configura injeção de dependência

---
+++ minimal-api/Dominio/Servicos/AdministradorServicos.cs
@@ -0,0 +1,23 @@
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.DTOs;
+using System.Data.Common;
+using MinimalApi.Infraestrutura.Db;
+
+namespace MinimalApi.Dominio.Servicos;
+
+public class AdministradorServico : IAdministradorServico
+{
+    private readonly DbContexto _contexto;
+    public AdministradorServico(DbContexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    public Administrador? Login(LoginDTO loginDTO)
+    {
+        var adm = _contexto.Administradores.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha).FirstOrDefault();
+        return adm;
+        
+    }
+}
--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 
 builder.Services.AddDbContext<DbContext>(options =>
 {
@@ -14,9 +21,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    if (administradorServico.Login(loginDTO) != null)
         return Results.Ok("Login realizado com sucesso!");
     else
         return Results.Unauthorized();
