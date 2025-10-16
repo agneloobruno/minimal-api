fc5795a91d8c4065e648245d5016d61264a00037
Bruno Agnelo De Almeida <3714194@empresa.mti.mt.gov.br>
2025-10-16 16:57:27 -0400

    configuração de testes

---
+++ Test/Domain/Servicos/AdministradorServico.cs
@@ -0,0 +1,52 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.Infraestrutura.Db;
+
+namespace Test.Domain.Entidades;
+
+[TestClass]
+public class AdministradorServicoTest
+{
+
+    private DbContexto CriarContextoTeste()
+    {
+        var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var path = Path.GetFullPath(Path.Combine(assemblyPath ?? "", "..", "..", "..", "..", "Api"));
+        
+        //Configurar o ConfigurationBuilder
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path ?? Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        var configuration = builder.Build();
+        
+        return new DbContexto(configuration);
+    }
+
+    [TestMethod]
+    public void TestandoSalvarAdministrador()
+    {
+        // Arrange
+        var context = CriarContextoTeste();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores");
+
+        var adm = new Administrador();
+        adm.Email = "teste@teste.com";
+        adm.Senha = "senha123";
+        adm.Perfil = "Adm";
+        
+        var administradorServico = new AdministradorServico(context);
+
+        // Act
+        administradorServico.Adicionar(adm);
+
+
+        // Assert
+        Assert.AreEqual(1, administradorServico.Todos(1).Count());
+    }
+}
